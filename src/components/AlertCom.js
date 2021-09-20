82d0b55bab0e66168b90208975a74cfbd7e59ec2
mohammadlebzo <mohlebzo1@gmail.com>
2021-09-20 18:43:39 +0300

    Added error handling

---
+++ src/components/AlertCom.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert'
+
+class AlertCom extends React.Component {
+
+    render() {
+        return (
+            <>
+
+                <Alert show={this.props.showError} variant="success">
+                    <Alert.Heading>Something went wrong in getting location data</Alert.Heading>
+                    <p>Status Code: 404</p>
+                </Alert>
+
+            </>
+        )
+    }
+}
+
+export default AlertCom;
