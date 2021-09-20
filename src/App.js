10ece30275f32ca56f4e91468912687d2b2ee61c
mohammadlebzo <mohlebzo1@gmail.com>
2021-09-20 23:06:18 +0300

    Finished lab07, the application receives and displays needed information

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FormCom from './components/FormCom';
 import CardCom from './components/CardCom';
 import TableCom from './components/TableCom';
 import AlertCom from './components/AlertCom';
+import Weather from './components/Weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -12,10 +13,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      code: '',
       locationResult: {},
       searchQuery: '',
       showLocInfo: false,
-      showError:false
+      showError: false
     }
   }
 
@@ -24,43 +26,52 @@ class App extends React.Component {
     await this.setState({
       searchQuery: event.target.city.value
     })
+    let serverUrl = `${process.env.REACT_APP_SERVER_LINK}/weather?city=${this.state.searchQuery}`
     try {
-      let reqUrl = `https://us1.locationiq.com/v1/searc.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      // let reqUrl = `https://us1.locationiq.com/v1/searc.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      
 
-      let locResult = await axios.get(reqUrl);
+      // let locResult = await axios.get(reqUrl);
+      let serverResult = await axios.get(serverUrl);
+
       this.setState({
-        locationResult: locResult.data[0],
+        locationResult: serverResult.data,
         showLocInfo: true,
-        showError:false
+        showError: false
       })
-      // console.log(locResult.status);
     } catch {
-      this.setState({
-        showError: true,
-        showLocInfo:false
-      })
+      fetch(serverUrl)
+        .then(response => {
+          this.setState({
+            code: response.status,
+            showError: true,
+            showLocInfo: false
+          })
+        })
     }
   }
 
-    render() {
-      return (
-        <div>
+  render() {
+    return (
+      <div>
 
-          <div className="left-box">
-            <FormCom locationInfo={this.getLocInformation} />
+        <div className="left-box">
+          <FormCom locationInfo={this.getLocInformation} />
 
-            {this.state.showLocInfo && <TableCom query={this.state.searchQuery} lat={this.state.locationResult.lat} lon={this.state.locationResult.lon} />}
-            {this.state.showError && <AlertCom showError={this.state.showError} />}
+          {this.state.showLocInfo && <TableCom col1={['City Name', 'Latitude:', 'Longitude:']} col2={[this.state.searchQuery, this.state.locationResult.lan, this.state.locationResult.lon]} />}
+          {this.state.showLocInfo && <Weather forcast={this.state.locationResult} />}
 
-          </div>
-
-          <div className="right-box">
-            {this.state.showLocInfo && <CardCom locationName={this.state.searchQuery} lat={this.state.locationResult.lat} lon={this.state.locationResult.lon} />}
-          </div>
+          {this.state.showError && <AlertCom showError={this.state.showError} message={this.state.code} />}
 
         </div>
-      )
-    }
+
+        <div className="right-box">
+          {this.state.showLocInfo && <CardCom locationName={this.state.searchQuery} lat={this.state.locationResult.lan} lon={this.state.locationResult.lon} />}
+        </div>
+
+      </div>
+    )
   }
+}
 
 export default App;
